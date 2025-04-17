a309aede366e77f7031435b5911041703bbced2d
Avinash <60576485+Maidira@users.noreply.github.com>
2025-04-17 17:44:46 +0530

    Update olist_analysis.sql

--- olist_analysis.sql
+++ olist_analysis.sql
@@ -24,28 +24,28 @@ RENAME COLUMN
 -- Repeat customers vs one time buyers
 with cte AS(
 	SELECT 
-		customer_id,
-        COUNT(order_id) AS order_count
+	   customer_id,
+           COUNT(order_id) AS order_count
 	FROM 
-    olist_orders_dataset
-  GROUP BY customer_id)
+    	   olist_orders_dataset
+ 	GROUP BY customer_id)
 SELECT
-	  COUNT(DISTINCT CASE WHEN order_count > 1 THEN customer_id END) AS repeat_customers,
+    COUNT(DISTINCT CASE WHEN order_count > 1 THEN customer_id END) AS repeat_customers,
     COUNT(DISTINCT CASE WHEN order_count = 1 THEN customer_id END) AS one_time_custmers
 FROM 
     cte;
 
 -- Average Order per customer by state
 SELECT 
-	c.customer_state,
+    c.customer_state,
     COUNT(order_id)/COUNT(DISTINCT o.customer_id) AS avg_orders_per_customer
 FROM 
-	olist_orders_dataset o 
+    olist_orders_dataset o 
 JOIN
-	olist_customers_dataset c
+    olist_customers_dataset c
     ON o.customer_id = c.customer_id
 GROUP BY 
-	c.customer_state
+    c.customer_state
 ORDER BY avg_orders_per_customer;
 
 
@@ -53,13 +53,13 @@ ORDER BY avg_orders_per_customer;
 
 -- Monthly Sales trend
 SELECT
-	  EXTRACT(MONTH FROM order_purchase_timestamp) AS order_month,
+    EXTRACT(MONTH FROM order_purchase_timestamp) AS order_month,
     COUNT(o.order_id) AS total_orders,
     SUM(price) AS total_revenue
 FROM 
     olist_orders_dataset o
 JOIN
-	  olist_order_items_dataset oi
+    olist_order_items_dataset oi
     ON o.order_id = oi.order_id
 GROUP BY 
     order_month
@@ -69,20 +69,19 @@ ORDER BY
 
 -- High revenue generating sellers
 SELECT
-	  s.seller_id,
+    s.seller_id,
     s.seller_city,
     ROUND(SUM(oi.price),2) AS total_revenue
 FROM 
     olist_sellers_dataset s
 JOIN 
     olist_order_items_dataset oi
-	  ON 
-    s.seller_id = oi.seller_id
+   ON s.seller_id = oi.seller_id
 GROUP BY 
-	  s.seller_id,
+    s.seller_id,
     s.seller_city
 ORDER BY
-	  total_revenue DESC;
+    total_revenue DESC;
 
 
 
